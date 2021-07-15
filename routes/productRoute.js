169b4c20e18b23c96b9f81d82cc8e3dd441a9663
Arihant <jarihant701@gmail.com>
2021-07-15 12:32:12 +0530

    Complete product routes

---
+++ routes/productRoute.js
@@ -0,0 +1,75 @@
+const router = require('express').Router();
+const Product = require('../model/Product');
+
+/* 
+  @desc     Get all product details
+  @route    /api/products/
+  @access   public
+*/
+router.get('/', async (req, res) => {
+  const sort = req.query.sort === 'desc' ? -1 : 1;
+  const data = await Product.find()
+    .limit(+req.query.limit)
+    .select('-_id -__v')
+    .sort({ id: sort });
+  res.send(data);
+});
+
+/* 
+  @desc     Get a product by id
+  @route    /api/products/:id
+  @access   public
+*/
+router.get('/:id', async (req, res) => {
+  const data = await Product.findOne({ id: req.params.id }).select('-_id -__v');
+  res.send(data);
+});
+
+/*
+  @desc     Create a new product
+  @route    /api/products/
+  @access   public
+*/
+router.post('/', async (req, res) => {
+  const productCount = await Product.countDocuments({});
+  const newId = productCount + 1;
+  const newProduct = new Product({
+    id: newId,
+    title: req.body.title,
+    description: req.body.description,
+    price: req.body.price,
+    category: req.body.category,
+    image: req.body.image,
+  });
+  await newProduct.save();
+  res.send(newProduct);
+});
+
+/*
+  @desc     Update a product
+  @route    /api/products/:id
+  @access   public
+*/
+router.put('/:id', async (req, res) => {
+  const data = await Product.findOneAndUpdate({ id: req.params.id }, req.body, {
+    new: true,
+  }).select('-_id -__v');
+  res.send(data);
+});
+
+/*
+  @desc     Delete a product
+  @route    /api/products/:id
+  @access   public
+*/
+router.delete('/:id', async (req, res) => {
+  const data = await Product.findOneAndRemove(
+    { id: req.params.id },
+    {
+      new: true,
+    }
+  ).select('-_id -__v');
+  res.send(data);
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,23 +1,35 @@
-//* dotenv
+//* importing modules
 require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+//* importing routes
+const productRoute = require('./routes/productRoute');
 
 //* express
-const express = require('express');
 const app = express();
 
 //* connect database
-const mongoose = require('mongoose');
 mongoose.connect(
   process.env.DB_URI,
-  { useNewUrlParser: true, useUnifiedTopology: true },
+  { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false },
   () => console.log('Database connection successful')
 );
 
 //* Variables
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  res.send('Hello');
-});
+//* Enable cors
+app.use(cors());
+
+//* Accept json in request body
+app.use(express.json());
+
+//* Serve static files
+app.use(express.static('public'));
+
+//* Routes
+app.use('/api/products/', productRoute);
 
 app.listen(PORT, () => console.log('App is running at ', PORT));
